4dff55ad2de6e8c7c6e06c05bd2230d4cf4816d1
Suhail Lala <suhail@example.com>
2017-05-30 15:57:38 +0530

    first commit

---
+++ src/main/java/mycompany/testing/com/App.java
@@ -0,0 +1,13 @@
+package mycompany.testing.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
